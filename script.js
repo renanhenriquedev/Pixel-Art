5be5fa2ddf5aa9ea619b1f5ae3ac746a230ffaa5
Renan <Renanhenrique20222@gmail.com>
2023-01-21 10:16:31 -0300

    requisition 3 done

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ sectionPalette.id = 'color-palette';
 body.appendChild(sectionPalette);
 
 for (let i = 0; i < 4; i += 1) {
-  const cores = ['red', 'blue', 'purple', 'yellow'];
+  const cores = ['black', 'blue', 'purple', 'yellow'];
   const pegarSection = document.querySelector('section');
   let criarQuadrado = document.createElement('div');
   criarQuadrado.classList.add('quadrado', cores[i], 'color');
